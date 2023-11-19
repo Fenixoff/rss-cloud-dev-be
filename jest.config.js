b79d5aa5ca28bdd576defdd30c00dd3ad15d35fd
Andrei Azhoichyk <azh.andrei@gmail.com>
2023-11-19 21:43:00 +0200

    fix: npm scripts for running tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: "node",
+  testMatch: ["**/test/**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  collectCoverageFrom: ["*/{lib,bin}/**/*.ts"],
+};
