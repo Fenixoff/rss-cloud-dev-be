b8d62cd5b4bffcc9b79752b647ff06c13c0fea06
Andrei Azhoichyk <azh.andrei@gmail.com>
2023-11-25 16:38:26 +0200

    feat: add common constants for product-service

---
+++ common/lib/constants.ts
@@ -0,0 +1,6 @@
+export const PRODUCT_SERVICE_API_DNS = "ProductServiceApiDns";
+
+export const PRODUCT_SERVICE_PRODUCT_TABLE_NAME =
+  "ProductServiceProductTableName";
+
+export const PRODUCT_SERVICE_STOCK_TABLE_NAME = "ProductServiceStockTableName";
