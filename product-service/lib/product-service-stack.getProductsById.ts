06f8ebc76d69798a95a33bb4bd7bfc769f65e697
Andrei Azhoichyk <azh.andrei@gmail.com>
2023-11-26 02:00:24 +0200

    feat(product-service): implement getProductsById to return data from DynamoDB

--- product-service/lib/product-service-stack.getProductsById.ts
+++ product-service/lib/product-service-stack.getProductsById.ts
@@ -1,21 +1,70 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
-import { buildResponse } from "./utils";
-import { products } from "../data/products";
+import { validate as uuidValidate } from "uuid";
+
+import { buildResponse, errorResponse } from "./utils";
+import { ddbDocClient } from "./clients/dynamodb";
+import { Product } from "./models/Product";
+import { TransactGetCommand } from "@aws-sdk/lib-dynamodb";
 
 export { getProductsById as handler };
 
 const getProductsById = async (
   event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyResultV2> => {
-  const id = event.pathParameters?.productId;
-  if (!id) {
-    return buildResponse(400, { message: "ProductId is required" });
+  try {
+    console.log("Request event:", event);
+
+    const id = event.pathParameters?.productId;
+    if (!id) {
+      return buildResponse(400, { message: "ProductId is required" });
+    }
+
+    if (!uuidValidate(id)) {
+      return buildResponse(400, { message: "ProductId is not valid" });
+    }
+
+    const selectedProduct = await getProductsByIdFromDb(id);
+    if (typeof selectedProduct === "undefined") {
+      return buildResponse(404, { message: "Product not found" });
+    }
+
+    return buildResponse(200, selectedProduct);
+  } catch {
+    return errorResponse;
+  }
+};
+
+const getProductsByIdFromDb = async (
+  id: string,
+): Promise<Product | undefined> => {
+  const { Responses: responses } = await ddbDocClient.send(
+    new TransactGetCommand({
+      TransactItems: [
+        {
+          Get: {
+            TableName: process.env.PRODUCT_TABLE,
+            Key: { id },
+          },
+        },
+        {
+          Get: {
+            TableName: process.env.STOCK_TABLE,
+            Key: { id },
+          },
+        },
+      ],
+    }),
+  );
+
+  if (!responses) {
+    throw new Error("Database error");
   }
 
-  const selectedProduct = products.find((product) => product.id === id);
-  if (typeof selectedProduct === "undefined") {
-    return buildResponse(404, { message: "Product not found" });
+  const [{ Item: product }, { Item: stock }] = responses;
+
+  if (!product || !stock) {
+    return undefined;
   }
 
-  return buildResponse(200, selectedProduct);
+  return { ...product, ...stock } as Product;
 };
