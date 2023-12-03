9800162453b621b1f722920c749b6f26c77a51dc
Andrei Azhoichyk <azh.andrei@gmail.com>
2023-12-03 23:51:20 +0200

    refactor(import-service): improve `import-service-stack.importFileParser`

--- import-service/lib/import-service-stack.importFileParser.ts
+++ import-service/lib/import-service-stack.importFileParser.ts
@@ -1,4 +1,4 @@
-import { S3Handler } from "aws-lambda";
+import { S3Event } from "aws-lambda";
 import { NodeJsClient } from "@smithy/types";
 import { v4 as uuidv4 } from "uuid";
 import { finished } from "node:stream/promises";
@@ -9,37 +9,47 @@ import * as s3 from "@aws-sdk/client-s3";
 
 export { importFileParser as handler };
 
-const importFileParser: S3Handler = async (event) => {
-  const client = new s3.S3Client() as NodeJsClient<s3.S3Client>;
+const client = new s3.S3Client() as NodeJsClient<s3.S3Client>;
 
-  for (const record of event.Records) {
+const importFileParser = async (event: S3Event) => {
+  const promises = event.Records.map(async (record) => {
     const obj = {
       Bucket: record.s3.bucket.name,
       Key: decodeURIComponent(record.s3.object.key.replace(/\+/g, " ")),
     };
 
-    const file = (await client.send(new s3.GetObjectCommand(obj))).Body;
-    if (file) {
-      await Promise.all([
-        parse(file),
+    return processFile(obj);
+  });
+  await Promise.all(promises);
+};
 
-        client.send(
-          new s3.CopyObjectCommand({
-            Bucket: obj.Bucket,
-            CopySource: encodeURIComponent(`${obj.Bucket}/${obj.Key}`),
-            Key: obj.Key.replace("uploaded/", `parsed/${uuidv4()}-`),
-          }),
-        ),
-      ]);
-      client.send(new s3.DeleteObjectCommand(obj));
-    }
+const processFile = async (obj: S3Object) => {
+  const file = (await client.send(new s3.GetObjectCommand(obj))).Body;
+  if (file) {
+    await Promise.all([
+      parse(file),
+
+      client.send(
+        new s3.CopyObjectCommand({
+          Bucket: obj.Bucket,
+          CopySource: encodeURIComponent(`${obj.Bucket}/${obj.Key}`),
+          Key: obj.Key.replace("uploaded/", `parsed/${uuidv4()}-`),
+        }),
+      ),
+    ]);
+    client.send(new s3.DeleteObjectCommand(obj));
   }
 };
 
 const parse = async (stream: NodeJS.ReadableStream) => {
   return finished(
-    stream.pipe(csv()).on("data", (data) => {
-      console.log(data);
-    }),
+    stream
+      .pipe(csv())
+      .on("data", (data) => {
+        console.log(data);
+      })
+      .on("error", (error) => {
+        console.error("Parsing error:", error);
+      }),
   );
 };
---
+++ import-service/lib/types.ts
@@ -0,0 +1,2 @@
+type S3ObjectKeys = "Bucket" | "Key";
+type S3Object = Record<S3ObjectKeys, string>;
