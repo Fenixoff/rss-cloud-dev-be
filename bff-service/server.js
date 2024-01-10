e1c9c97031c89e31410205fbc351161ea7319577
Andrei Azhoichyk <azh.andrei@gmail.com>
2024-01-10 17:10:11 +0200

    feat(bff-service): implement caching

--- bff-service/server.js
+++ bff-service/server.js
@@ -4,17 +4,27 @@ import httpProxy from "http-proxy";
 import "dotenv/config";
 
 const rules = JSON.parse(process.env.RULES);
+const cachingMsecs = 20000;
 
-const hostname = "0.0.0.0";
 const port = 3000;
 
 const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 const cache = {};
 
-proxy.on("proxyRes", (_proxyRes, req, res) => {
+proxy.on("proxyRes", (proxyRes, req, res) => {
   if (req.url === "/products") {
-    cache.res = res;
-    cache.time = Date.now();
+    const chunks = [];
+
+    proxyRes.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    proxyRes.on("end", () => {
+      cache.body = Buffer.concat(chunks);
+      cache.headers = proxyRes.headers;
+      cache.statusCode = proxyRes.statusCode;
+      cache.time = Date.now();
+    });
   }
 });
 
@@ -23,18 +33,21 @@ const server = createServer((req, res) => {
   const target = rules[service];
 
   if (target) {
-    // if (req.url === "/product/products" && cache.time > Date.now() - 20000) {
-
-    // }
-    req.url = path;
-    return proxy.web(req, res, { target });
+    if (
+      req.url === "/product/products" &&
+      cache.body &&
+      cache.time > Date.now() - cachingMsecs
+    ) {
+      res.writeHead(cache.statusCode, cache.headers).end(cache.body);
+    } else {
+      req.url = path;
+      proxy.web(req, res, { target });
+    }
+  } else {
+    res.statusCode = 502;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Cannot process request\n");
   }
-
-  res.statusCode = 502;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Cannot process request\n");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(port);
